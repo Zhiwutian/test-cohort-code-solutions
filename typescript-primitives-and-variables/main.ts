ee30b7d9c73c5028c10da769006f1453dc494d37
Brett Albright <balbright@learningfuze.com>
2024-01-08 21:40:45 +0000

    completed exercise

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,14 @@
+const fullName: string = 'Brett Albright';
+const isCool: boolean = true;
+const totalPets: number = 2;
+
+console.log('Fullname ', fullName);
+console.log('isCool ', isCool);
+console.log('totalPets ', totalPets);
+
+const unknown: null = null;
+let never: undefined;
+
+console.log('unknown ', unknown);
+
+console.log('never ', never);
